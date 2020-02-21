b222a8a0a600df5db1349c41cbb466b5f98f6b2b
Vikramaditya <vk92kokil@live.com>
2020-02-21 16:36:17 +0530

    add json file

--- express/index.js
+++ express/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const jsonServer = require('json-server');
 const middlewares = jsonServer.defaults();
-
+const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
+
 app.use(middlewares);
+app.use('public', express.static(path.join(process.cwd(), 'public')));
+app.get('/health', (req, res) => {
+   res.status(200).send({success: true});
+});
 app.use((req, res, next) => {
-   const router = jsonServer.router('db.json');
+   const router = jsonServer.router('public/db.json');
    router(req, res, next);
 });
 module.exports = app;
