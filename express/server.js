717d3dd29996d3afb18fc4628f875aeac93866f4
Vikramaditya <vk92kokil@live.com>
2020-02-21 17:38:00 +0530

    test

--- express/server.js
+++ express/server.js
@@ -4,18 +4,18 @@ const middlewares = jsonServer.defaults();
 const path = require('path');
 const serverless = require('serverless-http');
 
-const jsonRouter = jsonServer.router('public/db.json');
+// const jsonRouter = jsonServer.router('/db.json');
 const app = express();
 
 const router = express.Router();
 app.use(middlewares);
 
-app.use('public', express.static(path.join(process.cwd(), 'public')));
-router.get('/health', (req, res) => {
+// app.use('public', express.static(path.join(process.cwd(), 'public')));
+router.get('/', (req, res) => {
    res.status(200).send({success: true});
 });
 app.use('/.netlify/functions/health', router);
-app.use('/.netlify/functions/server', jsonRouter);
+// app.use('/.netlify/functions/server', jsonRouter);
 
 module.exports = app;
 module.exports.handler = serverless(app);
